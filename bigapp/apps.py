0d45155bd9914a93daef7790947564bfcd45c236
Amanda Simmons <carpe737diem@gmail.com>
2021-03-01 16:04:14 -0500

    successfully renamed my project and app to be consistent with the rubric

--- bigapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BigappConfig(AppConfig):
-    name = 'bigapp'
--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -1,4 +1,4 @@
-"""bigproject URL Configuration
+"""twitterclone URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bigapp import views
+from twitteruser import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
